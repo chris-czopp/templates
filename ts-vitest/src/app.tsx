e7ad07afe88862eda1aafbc5d840c827ae0b23f4
Krzysztof Czopp <chris@glue.codes>
2022-09-22 14:59:45 +0100

    more changes

--- ts-vitest/src/app.tsx
+++ ts-vitest/src/app.tsx
@@ -1,7 +1,12 @@
 import { createSignal, createResource, JSX } from 'solid-js';
+import axios from 'axios';
 
 const fetchUser = async (id: number) =>
-  (await fetch(`https://swapi.dev/api/people/${id}/`)).json();
+  (
+    await axios.get(
+      `${import.meta.env.VITE_API_BASE_URL || ''}/api/people/${id}/`,
+    )
+  ).data;
 
 export const App = (): JSX.Element => {
   const [userId, setUserId] = createSignal<number>(1);
