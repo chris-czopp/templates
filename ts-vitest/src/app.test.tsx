145627b0091c347a57f26a24afff86c336337fe3
Krzysztof Czopp <chris@glue.codes>
2022-09-22 17:06:28 +0100

    mocks and tests

--- ts-vitest/src/app.test.tsx
+++ ts-vitest/src/app.test.tsx
@@ -1,11 +1,32 @@
-import { describe, expect, test as it } from 'vitest';
-import { render, fireEvent, screen } from 'solid-testing-library';
+import { describe, expect, test as it, vi } from 'vitest';
+import { fireEvent, render, screen, waitFor } from 'solid-testing-library';
+import { getRequestSpy } from '../setupVitest';
 import { App } from './app';
 
-describe('<TodoList />', () => {
-  it('should fetch user data', () => {
-    const { unmount } = render(() => <App />);
+describe('user app', () => {
+  const waitForInitialRequests = async () => {
+    await waitFor(() =>
+      expect(getRequestSpy('200 GET /api/people/1/')).toHaveBeenCalledTimes(1),
+    );
+  };
 
+  it('should fetch data', async () => {
+    const initialProps = {};
+    const { unmount } = render(() => <App {...initialProps} />);
+
+    await waitForInitialRequests();
+    expect(screen.getByTestId('userData')).toMatchSnapshot();
+
+    fireEvent.input(screen.getByPlaceholderText('Enter Numeric Id'), {
+      target: {
+        value: '2',
+      },
+    });
+
+    await waitFor(() =>
+      expect(getRequestSpy('200 GET /api/people/2/')).toHaveBeenCalledTimes(1),
+    );
+    expect(screen.getByTestId('userData')).toMatchSnapshot();
     unmount();
   });
 });
